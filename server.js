d1b0655360d02691c1e2be185f65c410d3974651
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-02-07 23:24:55 +0100

    got the correct info from API

--- server.js
+++ server.js
@@ -2,30 +2,54 @@
 var fs = require('fs');
 // seperated auth for security reasons
 var auth = require('./auth');
-
 var http = require('http');
+var url = require('url');
 var SpotifyWebApi = require('spotify-web-api-node');
 
+// setup the spotify api
 spotifyApi = new SpotifyWebApi(auth.Authentication);
-spotifyApi.clientCredentialsGrant()
-    .then(function(data) {
-        console.log('Expires in ' + data.body['expires_in']);
-        console.log('Token = ' + data.body['access_token']);
-        spotifyApi.setAccessToken(data.body['access_token']);
-    }, function(err) {
-        console.log('Token failed', err);
-    });
-
-http.createServer(function(req, res) {
-    spotifyApi.searchTracks(search)
+function setCredentials() {
+    spotifyApi.clientCredentialsGrant()
         .then(function(data) {
-            // this gets the ui from the api response
-            return data.body.tracks.items.map(function(t) { return t.uri; });
+            console.log('Expires in ' + data.body['expires_in']);
+            console.log('Token = ' + data.body['access_token']);
+            spotifyApi.setAccessToken(data.body['access_token']);
+        }, function(err) {
+            console.log('Token failed', err);
+        });
+}
+
+// start server
+http.createServer(function(req, res) {
+    var q = url.parse(req.url, true).query;
+    spotifyApi.searchTracks(q.track)
+        .then(function(data) {
+
+            // this gets the relevant info from the api response
+            return data.body.tracks.items.map(function(t) {
+                var uri = t.uri;
+                var title = t.name;
+                var artists = t.artists.map(function(t) {
+                    return t.name;
+                });
+                return {
+                    Artists: String(artists),
+                    Title: title,
+                    URI: uri,
+                };
+            });
         })
         .then(function(data) {
+
+            // this data is the relevant information
             console.log(data);
         })
         .catch(function(error) {
-            console.error(error);
+            if (error == 'WebapiError: Unauthorized') {
+                setCredentials();
+            }
+            else {
+                console.error(error);
+            }
         });
 }).listen(8080);
