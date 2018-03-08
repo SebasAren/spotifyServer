0b900728e07fb657fe10bc11b1b000d4f8fed22b
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-03-08 15:16:54 +0100

    obscured the player

---
+++ dbusPlayer/settings.template.py
@@ -0,0 +1,4 @@
+BASE_URL = 'http://localhost:8080/' # while testing
+OVERVIEW_URL = BASE_URL + 'overview' # default
+CLEAR_URL = BASE_URL + 'clear/?pass=' # default
+PASS = 'somethingsomething' # change this to match node.js server
