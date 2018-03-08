bc737d36c906fd789c268c31782720479232dbed
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-03-08 14:40:00 +0100

    fixed authentication security

--- auth.js
+++
@@ -1,5 +0,0 @@
-exports.Authentication = {
-    clientId: '6c8fb8714d5c433b915ccda4a9bcb35b',
-    clientSecret: '2e40b3d52b1b4c8e93c07a9848ff5907',
-    redirectUri: 'http://localhost:8080/callback',
-}
