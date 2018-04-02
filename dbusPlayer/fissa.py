b5dc3e196587087d37cb8fbb3bf9353ab842d8be
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-04-02 15:55:22 +0200

    controller remake

--- dbusPlayer/fissa.py
+++ dbusPlayer/fissa.py
@@ -1,24 +1,8 @@
 import settings
-from mpris import MPRISRemote
 from urllib.request import urlopen
-import json
 
-BASE_URL = 'http://localhost:8080/'
-OVERVIEW_URL = BASE_URL + 'overview'
-CLEAR_URL = BASE_URL + 'clear/?pass='
-PASS = 'ditismijnservervreund'
-remote = MPRISRemote()
+def flush_queue():
+    response = urlopen(settings.CLEAR_URL + settings.PASS)
+    print(response.read())
 
-def get_json():
-    response = urlopen(OVERVIEW_URL)
-    data = json.load(response)
-    return data
-
-def reset():
-    urlopen(CLEAR_URL + PASS)
-
-def add_to_queue(data):
-    for song in data:
-        remote.add_spotify_song(song['URI'])
-
-add_to_queue(get_json())
+flush_queue()
