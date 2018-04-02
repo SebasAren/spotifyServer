0d27e9e74223adc747b2fc8c2b0f52ee4d90f950
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-04-02 22:59:33 +0200

    switched to curl instead of python

--- remoteControl/fissa.py
+++
@@ -1,5 +0,0 @@
-import settings
-from urllib.request import urlopen
-
-response = urlopen(settings.CLEAR_URL + settings.PASS)
-print(response.read())
--- remoteControl/settings.template.py
+++
@@ -1,4 +0,0 @@
-BASE_URL = 'http://localhost:8080/' # while testing
-OVERVIEW_URL = BASE_URL + 'overview' # default
-CLEAR_URL = BASE_URL + 'clear/?pass=' # default
-PASS = 'somethingsomething' # change this to match node.js server
