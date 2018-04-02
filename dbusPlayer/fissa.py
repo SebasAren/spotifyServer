8957b7536f68da41e8db96329e84e9fec664b133
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-04-02 19:16:41 +0200

    layout update

--- dbusPlayer/fissa.py
+++ dbusPlayer/fissa.py
@@ -1,8 +1,5 @@
 import settings
 from urllib.request import urlopen
 
-def flush_queue():
-    response = urlopen(settings.CLEAR_URL + settings.PASS)
-    print(response.read())
-
-flush_queue()
+response = urlopen(settings.CLEAR_URL + settings.PASS)
+print(response.read())
